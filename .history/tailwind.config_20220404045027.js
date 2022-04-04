f1fb6b173dfa1a39868ef442eec6390a918c0265
nitai <nitaibarran5@gmail.com>
2022-04-04 04:57:14 -0400

    this is some updates to the code, installed some packages to help with development

--- .history/tailwind.config_20220404045027.js
+++ .history/tailwind.config_20220404045027.js
@@ -2,15 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    debugScreens: {
-      position: ['top', 'left'],
-    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-debug-screens'),
+    require('tailwindcss-debug-screens')
   ],
 }
--- .history/tailwind.config_20220404045330.js
+++ .history/tailwind.config_20220404045330.js
@@ -2,9 +2,6 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    debugScreens: {
-      position: ['top', 'left'],
-    },
     extend: {},
   },
   variants: {
